2516f38031707a8c190b5ddbe43ef7eb0e5a6225
kento sasamoto <knote0721@gmail.com>
2015-11-09 17:55:16 +0900

    TF-IDFの実装

--- lib/tasks/morphological_analysis.rb
+++ lib/tasks/morphological_analysis.rb
@@ -10,12 +10,15 @@ natto = Natto::MeCab.new
 Entry.all.each do |en|
   next if en.words.present?
   position = 0
-  natto.parse(en.entry_content.content)
   natto.parse(en.entry_content.content) do |n|
     val = n.feature.split(',')
-    word = Word.create(surface_form: n.surface, pos: val[0], basic_form: val[4])
+    word = Word.where(surface_form: n.surface, pos: val[0], basic_form: val[4])
+    if word.blank?
+      word = Word.create(surface_form: n.surface, pos: val[0], basic_form: val[4])
+    else
+      word = word.first
+    end
     EntryWordRelation.create(entry_id: en.id, word_id: word.id, position: position)
     position += 1
   end
 end
-#
---
+++ lib/tasks/tf_idf.rb
@@ -0,0 +1,48 @@
+require 'pry'
+require 'pp'
+def getTF(entry)
+  tf = Hash.new(0)
+  n = 0
+  entry.entry_word_relations.each do |relation|
+    next unless relation.word.pos === "名詞"
+    n += 1
+    tf[relation.word.surface_form] += 1
+  end
+  tf.each do |key, val|
+    tf[key] = val/n.to_f
+  end
+  return tf
+end
+
+def getDF(entry, tf)
+  idf = Hash.new(0)
+  entries_count = Entry.count
+  tf.each do |key, val|
+    word = Word.find_by(surface_form: key, pos: "名詞")
+    idf[key] = Math.log(entries_count/word.entry_word_relations.count.to_f) + 1
+  end
+  return idf
+end
+
+def getTfIdf(tf, idf)
+  tf_idf = Hash.new(0)
+  tf.each do |key, val|
+    tf_idf[key] = val * idf[key]
+  end
+  return tf_idf
+end
+count = 0
+Keyword.delete_all
+Entry.all.each do |entry|
+  tf = getTF(entry)
+  idf = getDF(entry, tf)
+  tf_idf = getTfIdf(tf, idf)
+  tf_idf = tf_idf.sort_by{|k, v| v}.reverse
+  tf_idf.each_with_index do |val, n|
+    break if n >= 10
+    count += 1
+    puts count
+    word = Word.find_by(surface_form: val[0], pos: "名詞")
+    Keyword.create(entry_id: entry.id, word_id: word.id, score: val[1])
+  end
+end
